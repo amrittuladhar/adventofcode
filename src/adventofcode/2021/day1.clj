5130489926d2db1c6cdfb88b05c28870fe56c610
Amrit Tuladhar <amrit.tuladhar@disneystreaming.com>
2021-12-01 17:22:34 -0800

    Advent of Code 2021 - Day 1 - Small clean-up to reuse count-increases since it's ultimately all the same thing

--- src/adventofcode/2021/day1.clj
+++ src/adventofcode/2021/day1.clj
@@ -5,16 +5,14 @@
   ([] 0)
   ([current tuple] (if (> (second tuple) (first tuple)) (inc current) current)))
 
-(defn count-sum-increases
-  ([] 0)
-  ([current tuple] (if (> (reduce + (second tuple)) (reduce + (first tuple))) (inc current) current)))
-
 (defn solution []
   (let [day1-data (map u/to-int (u/read-file "./data/2021/day1/input.txt"))
         groups-of-two (u/partition-seq day1-data 2)
-        tuples-of-triples (u/partition-seq (u/partition-seq day1-data 3) 2)]
+        groups-of-three (u/partition-seq day1-data 3)
+        sums-of-triples (map #(reduce + %) groups-of-three)
+        groups-of-triple-sums (u/partition-seq sums-of-triples 2)]
     {
      :part1 (reduce count-increases 0 groups-of-two)
-     :part2 (reduce count-sum-increases 0 tuples-of-triples)
+     :part2 (reduce count-increases 0 groups-of-triple-sums)
      }
     ))
