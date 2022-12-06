9139d4cb09d01d68192e3eb9ab84e2d9c71a03a7
Amrit Tuladhar <amrit.tuladhar@disneystreaming.com>
2022-12-05 21:45:16 -0800

    2022 - Day 6

---
+++ src/adventofcode/2022/day6.clj
@@ -0,0 +1,19 @@
+(ns adventofcode.2022.day6)
+(require '[adventofcode.util :as u]
+         '[clojure.string :as str])
+
+(defn parse [file]
+  (let [lines (u/read-file (str "./data/2022/day6/" file ".txt"))]
+    (first lines)))
+
+(defn find-marker [message length]
+  (->> (partition length 1 message)
+      (take-while #(not (= (count %) (count (set %)))))
+      count
+      (+ length)))
+
+(defn part1 [file]
+  (find-marker (parse file) 4))
+
+(defn part2 [file]
+  (find-marker (parse file) 14))
